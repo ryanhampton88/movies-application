c3a15d86eac4eeab46ccea2e8b1adf8d0dd5b022
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-21 14:30:21 -0500

    making progress, switching spots

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ import {
     getMovie,
     postMovie,
     getLocalMovie,
-    renderMovie
+    renderMovie, addMovie
 } from "./movie-utils.js"
 
 
 const userTitleInput = document.getElementById("user-title");
 const userRatingInput = document.getElementById("user-rating");
-const addMovieButton = document.getElementById("add-movie-button");
+
+
 
 
 
@@ -27,28 +28,14 @@ const addMovieButton = document.getElementById("add-movie-button");
 
 
 
-    renderMovie(movie);
 
     let localMovies = await getLocalMovie()
-    console.log(localMovies);
     localMovies.forEach((localMovie) => {
         renderMovie(localMovie)
     })
 
-    addMovieButton.addEventListener("click", function() {
-        let newTitle = userTitleInput.value
-        console.log(newTitle);
-        let newRating = userRatingInput.value
-        console.log(newRating);
-        let newMovieObj = {
-            "Title": `${newTitle}`,
-            "Ratings": `${newRating}`,
-            "Genre": "",
-            "Plot": ""
-        }
-        postMovie(newMovieObj)
 
-    })
+    addMovie()
 
 
     /////////
