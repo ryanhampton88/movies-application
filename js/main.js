114e04e0df3683c86059a7825a0d3ce9668ace68
Ryan Hampton <hamptonryan88@gmail.com>
2023-09-22 10:30:54 -0500

    finished finished functionalities

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {
-    getMovie, postMovie, getLocalMovie, renderMovie, searchMovie
+    getLocalMovie, searchMovie
 } from "./movie-utils.js";
 import {strToStandardCase} from "./js-utils.js";
 
@@ -8,15 +8,7 @@ const addMovieButton = document.getElementById("add-movie-button");
 //////// MAIN METHOD/////////
 (async () => {
 
-
-    let localMovies = await getLocalMovie();
-    localMovies.forEach((localMovie) => {
-        console.log(localMovie);
-        let test = localMovie.Ratings.split("/")[0]
-        console.log(test);
-
-        renderMovie(localMovie);
-    });
+    getLocalMovie();
 
 
     addMovieButton.addEventListener("click", () => searchMovie())
@@ -24,8 +16,6 @@ const addMovieButton = document.getElementById("add-movie-button");
 
 })();
 //TODO: fix add movie form, directions require ability for user to enter movie title AND rating
-//TODO: actually DELETE movie from DB when delete button is clicked
 //TODO: fix edit button functionality, use star rating system???
-//TODO: loading screen, display laoding message, remove loading message when fetch request in obatined
 //TODO: make filter by gene tabs functional
 
